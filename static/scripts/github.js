cd2e55fb6120bf258235763eee3ff1a05653b69c
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-08-15 01:01:38 +0530

    Add Github widget UI and features js

---
+++ static/scripts/github.js
@@ -0,0 +1,16 @@
+fetch("https://api.github.com/users/navjot369", {
+    headers: {
+        'Authorization' : "token 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"
+    }
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    document.getElementById("github-dp").setAttribute("src", data.avatar_url);
+    document.getElementById("github-full-name").innerHTML = data.name;
+    document.getElementById("github-username").innerHTML = "@" + data.login;
+    document.getElementById("github-username").href = data.url;
+    document.getElementById("github-followers").innerHTML = data.followers;
+    document.getElementById("github-followings").innerHTML = data.following;
+});
+  
---
+++ static/scripts/leetcode.js
@@ -0,0 +1,4 @@
+fetch("https://leetcode-stats-api.herokuapp.com/navjot369")
+  .then(response => response.json())
+  .then(data => console.log(data));
+  
