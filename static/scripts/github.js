898dac57bf191b8c4d6dc4dfc9dea7c2854274fe
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-08-31 23:39:10 +0530

    Bug Fix: Grid layout in widgets

--- static/scripts/github.js
+++ static/scripts/github.js
@@ -5,7 +5,6 @@ fetch("https://api.github.com/users/navjot369", {
 })
   .then(response => response.json())
   .then(data => {
-    console.log(data);
     document.getElementById("github-dp").setAttribute("src", data.avatar_url);
     document.getElementById("github-full-name").innerHTML = data.name;
     document.getElementById("github-username").innerHTML = "@" + data.login;
