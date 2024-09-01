daf09abf03781b1de33d0d482b2867c310fc097b
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-09-01 13:44:14 +0530

    Feat: Sublinks available with animation ✨

--- static/scripts/data.js
+++ static/scripts/data.js
@@ -10,10 +10,103 @@ async function fetchData() {
     }
 }
 
-function showCont() {
-    document.getElementById("main-container").style.gridTemplateColumns = "1fr 1fr 2fr";
-}
+let OuterCont;
 
+function showSubLinks(title) {
+    //if that sublinks are already opened
+    let titleEle = document.querySelector(".sub-link-title-text");
+    if(titleEle?.innerHTML == title) {
+        return;
+    }
+
+    let prevEle = document.querySelector(".sub-links-inner");
+    
+    let fragment = new DocumentFragment();
+    let links=  data[title].cont;
+    let delay = 100;
+    for(let site in links) {
+        let outerA = document.createElement("a");
+        outerA.className = "link-outer-a";
+        outerA.href = links[site].link;
+
+        let innerA = document.createElement("div");
+        innerA.className = "link-inner-cont";
+
+        let imgCont = document.createElement("div");
+        imgCont.className = "link-icon-cont";
+        let imgEle = document.createElement("img");
+        imgEle.alt = "Icon";
+        imgEle.className = "link-icon";
+        imgEle.src= links[site].icon;
+        imgCont.appendChild(imgEle);
+
+        let nameEle = document.createElement("div");
+        nameEle.className = "link-name";
+        nameEle.innerText = site;
+
+        innerA.appendChild(imgCont);
+        innerA.appendChild(nameEle);
+
+        outerA.appendChild(innerA);
+        outerA.style.animationDelay = delay + "ms";
+        delay += 30;
+
+        fragment.appendChild(outerA);
+    }
+
+
+    if(prevEle) { //if it is already there
+        let nodeList = prevEle.childNodes;
+        Array.from(nodeList).forEach((ele) => {
+            if(ele.nodeName == "A") {
+                prevEle.removeChild(ele);
+            }
+        })
+        titleEle.innerHTML = title;
+        prevEle.append(fragment);
+
+
+    }else{ //to add from whole new context
+        let mainCont = document.getElementById("main-container");
+        mainCont.style.gridTemplateColumns = "1fr 1fr 2fr";
+        let subLinkCont = document.getElementById("sub-links-cont");
+
+        let outerCont = document.createElement("div");
+        outerCont.className = "sub-links-inner";
+
+        let subTitle = document.createElement("div");
+        subTitle.className = "sub-title";
+
+        let backButton = document.createElement("back-button");
+        backButton.className = "back-button";
+        let backImg = document.createElement("img");
+        backImg.src = "./icons/arrow-next.svg";
+        backImg.className = "sub-link-back-icon";
+        backImg.alt = "Back button";
+        backImg.onclick = () => {
+            subLinkCont.removeChild(outerCont);
+            mainCont.style.gridTemplateColumns = "2fr 1fr 3fr";
+        }
+        backButton.appendChild(backImg);
+
+        let titleCont = document.createElement("span");
+        titleCont.innerHTML = title;
+        titleCont.className = "sub-link-title-text";
+
+        subTitle.appendChild(backButton);
+        subTitle.appendChild(titleCont);
+
+        fragment.prepend(subTitle);
+
+        outerCont.appendChild(fragment);
+
+        subLinkCont.appendChild(outerCont);
+    }
+
+    // console.log(links);
+
+
+}
 
 fetchData().then(() => {
     console.log(data);
@@ -29,14 +122,13 @@ fetchData().then(() => {
     arrowImg.alt = "Arrow ->";
     arrowEle.appendChild(arrowImg);
 
+    let fragment = new DocumentFragment();
 
-    for(title in data) {
+
+    for(let title in data) {
         let curr = data[title];
         let buttonOuter = document.createElement("button");
         buttonOuter.className = "link-outer-button";
-        buttonOuter.onclick = (e) => {
-             console.log(e);
-        }
 
         let outerCont = document.createElement("div");
         outerCont.className = "link-inner-cont";
@@ -58,23 +150,29 @@ fetchData().then(() => {
 
         buttonOuter.appendChild(outerCont);
         buttonOuter.appendChild(arrowEle.cloneNode(true));
+        console.log(title);
 
-        container.appendChild(buttonOuter);
+        buttonOuter.id = title;
+        buttonOuter.onclick = function(e) {
+                showSubLinks(title);
+        }
 
-    }
+        fragment.append(buttonOuter);
+    };
 
+    container.append(fragment);
 
     // Final output to append
-    //     <button class="link-outer-button" onclick="showCont(${ele.title})">
-    //             <div class="link-inner-cont">
-    //                 <div class="link-icon-cont"><img src=${ele.icon} alt="Icon" class="link-icon">
-    //                 </div>
-    //                 <div class="link-name">${ele.title}</div>
-    //             </div>
-    //             <div class="arrow-outer">
-    //                 <img src="./icons/arrow-next.svg" class="arrow-inner" alt="arrow">
-    //             </div>
-    //         </button>
+        // <button class="link-outer-button" onclick="showCont(${ele.title})">
+        //         <div class="link-inner-cont">
+        //             <div class="link-icon-cont"><img src=${ele.icon} alt="Icon" class="link-icon">
+        //             </div>
+        //             <div class="link-name">${ele.title}</div>
+        //         </div>
+        //         <div class="arrow-outer">
+        //             <img src="./icons/arrow-next.svg" class="arrow-inner" alt="arrow">
+        //         </div>
+        //     </button>
     
 });
 
